aa8d39c257d8d0db7681885a406e2f34fc9e51d8
Anxcye <anxcye@outlook.com>
2026-03-19 15:11:33 +0800

    fix(build): add flutter clean and build_runner command to build script

--- buildohos.sh
+++ buildohos.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
+flutter clean
+
+dart run build_runner build --delete-conflicting-outputs                                                                                                                     ─╯
 
 flutter build app --release --dart-define=isOhosStore=true
